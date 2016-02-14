ad0e3f4e3a1444be5e736728d9c64d82c1b9ec07
Takeru Komiya <tkomiya@hotmail.com>
2016-02-13 17:01:14 -0800

    Adds javascript to resize sun height

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var sun = $('.sun');
+function resizeSun() {
+  sun.height(sun.width());
+}
+
+$(document).ready(function() {
+  resizeSun();
+  $(window).resize(function() {
+    resizeSun();
+    console.log(sun.height());
+  });
+});
